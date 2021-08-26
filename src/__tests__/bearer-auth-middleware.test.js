e1c87e9dfd257fa41b4962d2a3e8f70cd362e840
Rachnicrice <46403371+Rachnicrice@users.noreply.github.com>
2021-08-25 20:59:21 -0400

    refactoring

---
+++ src/__tests__/bearer-auth-middleware.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+require('dotenv').config();
+const middleware = require('../middleware/bearer.js');
+const { db, users } = require('../models');
+const jwt = require('jsonwebtoken');
+
+let testUsers = {
+  admin: { username: 'admin', password: 'password', role: 'admin' },
+};
+
+beforeAll(async (done) => {
+  await db.sync();
+  await users.create(testUsers.admin);
+  done();
+});
+afterAll(async (done) => {
+  await db.drop();
+  done();
+})
+
+describe('Auth Middleware', () => {
+  const req = {};
+  const res = {
+    status: jest.fn(() => res),
+    send: jest.fn(() => res)
+  }
+  const next = jest.fn();
+
+  describe('user authentication', () => {
+
+    it('fails a login for a user (admin) with an incorrect token', () => {
+
+      req.headers = {
+        authorization: 'Bearer thisisabadtoken',
+      };
+
+      return middleware(req, res, next)
+        .then(() => {
+          expect(next).not.toHaveBeenCalled();
+          expect(res.status).toHaveBeenCalledWith(403);
+        });
+
+    });
+
+    it('logs in a user with a proper token', () => {
+
+      const user = { username: 'admin' };
+      const token = jwt.sign(user, process.env.SECRET || 'secretstring');
+
+      req.headers = {
+        authorization: `Bearer ${token}`,
+      };
+
+      return middleware(req, res, next)
+        .then(() => {
+          expect(next).toHaveBeenCalledWith();
+        });
+
+    });
+
+  });
+
+});
